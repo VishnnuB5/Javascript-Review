550739e4672e00cc432f3e32daa28cdd98fb6c1e
VishnnuB5 <75132427+VishnnuB5@users.noreply.github.com>
2021-01-12 14:51:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var a="Vishnnu.B"
+
+console.log(a.toUpperCase());
+
+console.log(parseInt("5"))
+
+console.log(5+65);
+console.log(9-9)
+console.log(50*9)
+console.log(50/88)
+
+console.log(50%6)
+
+console.log(50>45)
+console.log(510<90)
+console.log(50!=50)
+console.log(50===50)
+
+if(3>2){
+  console.log("3 is greater than 2")
+}
+else{
+  console.log("2 is greater than 3")
+}
+
+for(var i=0; i<=5;i++){
+  console.log(i);
+}
+
+var l=10;
+while(l<=20){
+  console.log(l)
+  l=l+1
+  
+}
+
+var colours=["red","yellow","orange","blue","black"]
+colours.push("pink")
+colours.pop();
+console.log(colours);
+
+function multiply(a,b){
+product=a*b
+return product
+
+}
+console.log(multiply(20,60))
+
+function perimeter(h,w){
+  Ans=2*(h+w)
+  return Ans
+}
+
+console.log(perimeter(2,5))
+
+
+
